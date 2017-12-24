5aad1672ade0a41764973379567dabcea0a34162
Ivo Karaneshev <ivo.i.karaneshev@gmail.com>
2017-12-24 18:54:46 +0200

    Update function_pointer.c

--- pointers/function_pointer.c
+++ pointers/function_pointer.c
@@ -1,10 +1,21 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include<math.h>
 
 int sum(int a, int b)
 {
 	return a+b;
+}
+
+int mul(int a, int b)
+{
+	return a*b;
+}
+
+int abs_sub(int a, int b)
+{
+	return abs(a-b);
 }
 
 int main(int argc, char **argv)
@@ -13,5 +24,8 @@ int main(int argc, char **argv)
 	func_ptr = sum;
 	// same as funct_ptr = &sum;
 	printf("%d\n", func_ptr(4,6));
+	int (*arr_func[3])(int, int) = {sum, mul, abs_sub};
+	for(int i = 0; i < 3; i++)
+		printf("%d ", arr_func[i](10, 7));
 	return 0;
 }

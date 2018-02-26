969b4ad4004835eff40054bf915b83b032ee0375
Ivo Karaneshev <ivo.i.karaneshev@gmail.com>
2018-02-26 17:06:46 +0200

    Add files via upload

---
+++ external_function_call/function.h
@@ -0,0 +1,9 @@
+
+const float pi = 3.14;
+
+extern int zero;
+
+int sum(int a, int b)
+{
+  return a + b;
+}
---
+++ external_function_call/some_file.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include "function.h"
+
+int main()
+{
+  printf("%.2f\n", pi);
+  printf("%d\n", sum(12, 18));
+  return 0;
+}

8c3ac8a132956abf93001b78b5c24ef363517346
Ivo Karaneshev <ivo.i.karaneshev@gmail.com>
2018-03-25 16:10:21 +0300

    Update union.c

--- masks/union.c
+++ masks/union.c
@@ -6,7 +6,7 @@ int released(int old_state, int new_state);
 
 int main(int argc, char **argv)
 {
-	int old = 10;           // 1010
+  int old = 10;           // 1010
   int new = 13;           // 1101
   new_pressed(old, new);  // 0101
   released(old, new);     // 0010

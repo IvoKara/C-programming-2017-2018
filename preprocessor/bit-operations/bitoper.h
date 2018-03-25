febf365677bc97ab16edc382655f47e6adf20f54
Ivo Karaneshev <ivo.i.karaneshev@gmail.com>
2018-03-25 16:07:38 +0300

    Add files via upload

---
+++ preprocessor/bit-operations/bitoper.h
@@ -0,0 +1,21 @@
+#ifndef TURN_TO_BITS
+#define TURN_TO_BITS
+
+int *turn_to_bits(int n);
+
+#endif
+
+#ifndef PRINT_BITS
+#define PRINT_BITS
+
+void print_bits(int n);
+
+#endif
+
+#ifndef COUNT_ONES
+#define COUNT_ONES
+
+int count_ones(int n);
+
+#endif
+
---
+++ preprocessor/bit-operations/bits.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include "bitoper.h"
+
+
+int main(int argc, char **argv)
+{
+	print_bits(15);
+	return 0;
+}
